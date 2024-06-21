298d203ec1b4f7f3f39f5067e0310042365c16ad
Ratnadeep <ratnadeepdirsipam@gmail.com>
2024-06-21 18:25:46 +0530

    be a client

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,7 +8,7 @@ import { ActivatedRoute, Router} from '@angular/router'
 export class HomeComponent implements OnInit {
 
   routeNav:any = [
-    {name:"Be an Client",url:"/home/client",class:"client"},
+    {name:"Be an Astrologer",url:"/home/client",class:"client"},
     {name:"Search Astrologers",url:"/home/astrologers",class:"astrologer"},
     {name:"Contact us",url:"/home/contactus",class:"contactus"},
     {name:"Languages",url:"/home/client",class:"languages"},
@@ -19,16 +19,30 @@ export class HomeComponent implements OnInit {
   constructor(private route: ActivatedRoute, private router: Router){
 
   }
+
+  clientastrologer()
+  {
+    setTimeout(() => {
+      console.log("this.router.url",this.router.url)
+    this.router.url == "/home/client"?this.routeNav[0].name = "Be an Client":this.routeNav[0].name = "Be an Astrologer"
+    }, 100);
+  }
+
   ngOnInit(): void {
     // this.gotoHoe()
+    this.clientastrologer()
     let Jyotishhbody:any = document.getElementsByClassName('Jyotishh-body')
     if(Jyotishhbody[0]){ Jyotishhbody[0].style = `overflow-x:hidden;`}
   }
-  gotoHoe(){
+  gotoHome(){
+    this.clientastrologer()
     this.router.navigate(["/dashboard"], {relativeTo:this.route});
   }
   pageNavigation(page:string)
   {
+    this.clientastrologer()
+    if(page == "/home/client"){this.routeNav[0].name = "Be an Client"}
+    // else{this.routeNav.filter(())}
     this.router.navigate([page], {relativeTo:this.route});
   }
 
